7df3824bf9c33c339ff8439d0e3b27fe5d12f18c
pongsak <pkhamdee@pivotal.io>
2019-04-02 03:10:13 +0700

    a

--- build.sh
+++ build.sh
@@ -56,7 +56,7 @@ dockerLogin () {
             errorExit "Docker credentials not set (DOCKER_USR and DOCKER_PSW)"
         fi
 
-        docker --insecure-registry=harbor.pks.pkhamdee.com login ${DOCKER_REG} -u ${DOCKER_USR} -p ${DOCKER_PSW} || errorExit "Docker login to ${DOCKER_REG} failed"
+        docker login ${DOCKER_REG} -u ${DOCKER_USR} -p ${DOCKER_PSW} || errorExit "Docker login to ${DOCKER_REG} failed"
     else
         echo "Docker registry not set. Skipping"
     fi
@@ -85,7 +85,7 @@ buildDockerImage () {
 pushDockerImage () {
     echo -e "\nPushing ${DOCKER_REPO}:${DOCKER_TAG}"
 
-    docker push ${DOCKER_REG}/${DOCKER_REPO}:${DOCKER_TAG} || errorExit "Pushing ${DOCKER_REPO}:${DOCKER_TAG} failed"
+    docker --insecure-registry harbor.pks.pkhamdee.com push ${DOCKER_REG}/${DOCKER_REPO}:${DOCKER_TAG} || errorExit "Pushing ${DOCKER_REPO}:${DOCKER_TAG} failed"
 }
 
 # Packing the helm chart
